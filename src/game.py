bd8d0b677ee2b4c8fbf3c36bd54bce40eb37b48b
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-04-23 00:37:18 +0700

    Refractor and Re-Design class
    re-design class to make it much more sense.

---
+++ src/game.py
@@ -0,0 +1,163 @@
+import random
+
+class Game:
+
+    def __init__(self):
+        self._isStart = False
+        self._isOver = False
+        self._lstPlayer = []
+        self._lstAvailableChoice = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+        self._dictIndexToButtonName = { 1: "btn1", 2: "btn2", 3: "btn3", 4: "btn4", 5: "btn5", 6: "btn6", 7: "btn7", 8: "btn8", 9: "btn9" }
+
+
+    @property
+    def isStart(self):
+        return self._isStart
+
+    @property
+    def isOver(self):
+        return self._isOver
+
+    @property
+    def lstPlayer(self):
+        return self._lstPlayer
+     
+    @property
+    def lstAvailableChoice(self):
+        return self._lstAvailableChoice
+
+    @property
+    def dictIndexToButtonName(self):
+        return self._dictIndexToButtonName
+
+
+    @isStart.setter
+    def isStart(self, value):
+        self._isStart = value
+
+    @isOver.setter
+    def isOver(self, value):
+        self._isOver = value
+
+
+    def add_player(self, lstPlayer):
+        
+        for player in lstPlayer:
+            self._lstPlayer.append(player)
+
+
+    def is_all_in(self, lstExpect, lstNum):
+
+        isAllIn = True
+
+        for number in lstExpect:
+            isNumberInList = number in lstNum
+
+            if (not isNumberInList):
+                isAllIn = False
+                break
+
+        return isAllIn
+
+
+    def check_winner(self):
+
+        for player in self._lstPlayer:
+
+            if (self.is_all_in( [ 1, 2, 3 ], player.lstSelectedNum )):
+                player.win()
+
+            elif (self.is_all_in( [ 4, 5, 6 ], player.lstSelectedNum )):
+                player.win()
+
+            elif (self.is_all_in( [ 7, 8, 9 ], player.lstSelectedNum )):
+                player.win()
+
+            elif (self.is_all_in( [ 1, 4, 7 ], player.lstSelectedNum )):
+                player.win()
+
+            elif (self.is_all_in( [ 2, 5, 8 ], player.lstSelectedNum )):
+                player.win()
+
+            elif (self.is_all_in( [ 3, 6, 9 ], player.lstSelectedNum )):
+                player.win()
+
+            elif (self.is_all_in( [ 1, 5, 9 ], player.lstSelectedNum )):
+                player.win()
+
+            elif (self.is_all_in( [ 3, 5, 7 ], player.lstSelectedNum )):
+                player.win()
+
+
+    def start(self):
+        self._isStart = True
+
+
+    def over(self):
+        self._isOver = True
+
+
+class Player:
+
+    def __init__(self, name, marking):
+        self._name = name
+        self._marking = marking
+        self._isWin = False
+        self._isTurn = False
+        self._lstSelectedNum = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def marking(self):
+        return self._marking
+
+    @property
+    def isWin(self):
+        return self._isWin
+
+    @property
+    def isTurn(self):
+        return self._isTurn
+
+    @property
+    def lstSelectedNum(self):
+        return self._lstSelectedNum
+
+
+    @marking.setter
+    def marking(self, value):
+        self._marking = value
+
+    @isWin.setter
+    def isWin(self, value):
+        self._isWin = value
+
+    @isTurn.setter
+    def isTurn(self, value):
+        self._isTurn = value
+
+
+    def start_first(self):
+        self._isTurn = True
+
+    def win(self):
+        self._isWin = True
+
+    def lose(self):
+        self._isWin = False 
+
+
+class Bot(Player):
+
+    def random_pick(self, lstNum):
+
+        randomNum = 0
+
+        if (len(lstNum) > 0):
+            index = random.randint(0, len(lstNum) - 1)
+            randomNum = lstNum[index]
+
+        return randomNum
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-import actor
-from actor import Actor
-
+import game
 import kivy
+
 kivy.require("1.9.1")
+
+from game import Game
+from game import Player
+from game import Bot
 
 from kivy.app import App
 from kivy.config import Config
@@ -15,78 +18,38 @@ from kivy.uix.popup import Popup
 
 class TicTacToeGame(Widget):
 
-	isGameOver = False
+	game = Game()
+	player = Player("Player", "X")
+	enemy = Bot("Computer", "O")
 	
-	player = Actor("Player", False, "X")
-	enemy = Actor("Computer", True, "O")
+	game.add_player( [player, enemy] )
 	
-	lstAvailableChoice = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-	dictIndexToButtonName = { 1: "btn1", 2: "btn2", 3: "btn3", 4: "btn4", 5: "btn5", 6: "btn6", 7: "btn7", 8: "btn8", 9: "btn9" }
-
 	player.start_first()
-	
-	
-	def is_all_in(self, lstExpect, lstNum):
-		
-		isAllIn = True
-
-		for number in lstExpect:
-			isNumberInList = number in lstNum
-			
-			if (not isNumberInList):
-				isAllIn = False
-				break
-
-		return isAllIn
+	game.start()
 
 
-	def check_winner(self, actor):
-
-		if (self.is_all_in([ 1, 2, 3 ], actor.lstSelectedNum)):
-			actor.win()
-			
-		elif (self.is_all_in([ 4, 5, 6 ], actor.lstSelectedNum)):
-			actor.win()
-			
-		elif (self.is_all_in([ 7, 8, 9 ], actor.lstSelectedNum)):
-			actor.win()
-			
-		elif (self.is_all_in([ 1, 4, 7 ], actor.lstSelectedNum)):
-			actor.win()
-			
-		elif (self.is_all_in([ 2, 5, 8 ], actor.lstSelectedNum)):
-			actor.win()
-			
-		elif (self.is_all_in([ 3, 6, 9 ], actor.lstSelectedNum)):
-			actor.win()
-			
-		elif (self.is_all_in([ 1, 5, 9 ], actor.lstSelectedNum)):
-			actor.win()
-			
-		elif (self.is_all_in([ 3, 5, 7 ], actor.lstSelectedNum)):
-			actor.win()
-
-							
 	def set_all_button_disable(self, isDisable):
 		
-		for index in range(1, len(self.dictIndexToButtonName) + 1):
-			self.ids[ self.dictIndexToButtonName[index] ].disabled = isDisable
+		for index in range(1, len(self.game.dictIndexToButtonName) + 1):
+			self.ids[ self.game.dictIndexToButtonName[index] ].disabled = isDisable
   
 
 	def set_all_button_text(self, value):
 		
-		for index in range(1, len(self.dictIndexToButtonName) + 1):
-			self.ids[ self.dictIndexToButtonName[index] ].text = value
+		for index in range(1, len(self.game.dictIndexToButtonName) + 1):
+			self.ids[ self.game.dictIndexToButtonName[index] ].text = value
 		
 		
 	def restart_game(self, btn):
 		
-		self.isGameOver = False
-		self.player = Actor("Player", False, "X")
-		self.enemy = Actor("Enemy", True, "O")
-		self.lstAvailableChoice = list(self.dictIndexToButtonName.keys())
+		self.game = Game()
+		self.player = Player("Player", "X")
+		self.enemy = Bot("Enemy", "O")
+		
+		self.game.add_player([ self.player, self.enemy ])
 		
 		self.player.start_first()
+		self.game.start()
 		
 		self.set_all_button_text("")
 		self.set_all_button_disable(False)
@@ -94,74 +57,72 @@ class TicTacToeGame(Widget):
 
 	def button_press(self, btn):
 		
-		if (not self.isGameOver and self.player.isTurn):
+		if (not self.game.isOver and self.player.isTurn):
 			
 			selectedNum = 0
 		
-			if (btn == self.ids[ self.dictIndexToButtonName[1] ]):
+			if (btn == self.ids[ self.game.dictIndexToButtonName[1] ]):
 				selectedNum = 1
 		
-			elif (btn == self.ids[ self.dictIndexToButtonName[2] ]):
+			elif (btn == self.ids[ self.game.dictIndexToButtonName[2] ]):
 				selectedNum = 2
 			
-			elif (btn == self.ids[ self.dictIndexToButtonName[3] ]):
+			elif (btn == self.ids[ self.game.dictIndexToButtonName[3] ]):
 				selectedNum = 3
 		
-			elif (btn == self.ids[ self.dictIndexToButtonName[4] ]):
+			elif (btn == self.ids[ self.game.dictIndexToButtonName[4] ]):
 				selectedNum = 4
 		
-			elif (btn == self.ids[ self.dictIndexToButtonName[5] ]):
+			elif (btn == self.ids[ self.game.dictIndexToButtonName[5] ]):
 				selectedNum = 5
 		
-			elif (btn == self.ids[ self.dictIndexToButtonName[6] ]):
+			elif (btn == self.ids[ self.game.dictIndexToButtonName[6] ]):
 				selectedNum = 6
 		
-			elif (btn == self.ids[ self.dictIndexToButtonName[7] ]):
+			elif (btn == self.ids[ self.game.dictIndexToButtonName[7] ]):
 				selectedNum = 7
 		
-			elif (btn == self.ids[ self.dictIndexToButtonName[8] ]):
+			elif (btn == self.ids[ self.game.dictIndexToButtonName[8] ]):
 				selectedNum = 8
 		
-			elif (btn == self.ids[ self.dictIndexToButtonName[9] ]):
+			elif (btn == self.ids[ self.game.dictIndexToButtonName[9] ]):
 				selectedNum = 9
 
 
 			self.player.lstSelectedNum.append(selectedNum)
-			self.lstAvailableChoice.remove(selectedNum)
+			self.game.lstAvailableChoice.remove(selectedNum)
 			
 			btn.text = self.player.marking
 			btn.disabled = True
 			
 			
-			self.check_winner(self.player)
-			self.check_winner(self.enemy)
+			self.game.check_winner()
 			
 			
-			if (self.player.isWin or self.enemy.isWin or len(self.lstAvailableChoice) == 0):
-				self.isGameOver = True
+			if (self.player.isWin or self.enemy.isWin or len(self.game.lstAvailableChoice) == 0):
+				self.game.over()
 
 
 			self.player.isTurn = False
 			self.enemy.isTurn = True
 
 
-		if (not self.isGameOver and self.enemy.isTurn):
-			selectedNum = self.enemy.random_pick(self.lstAvailableChoice)
+		if (not self.game.isOver and self.enemy.isTurn):
+			selectedNum = self.enemy.random_pick(self.game.lstAvailableChoice)
 
 			if (selectedNum > 0):
 				self.enemy.lstSelectedNum.append(selectedNum)
-				self.lstAvailableChoice.remove(selectedNum)
+				self.game.lstAvailableChoice.remove(selectedNum)
 				
-				self.ids[ self.dictIndexToButtonName[selectedNum] ].text = self.enemy.marking
-				self.ids[ self.dictIndexToButtonName[selectedNum] ].disabled = True
+				self.ids[ self.game.dictIndexToButtonName[selectedNum] ].text = self.enemy.marking
+				self.ids[ self.game.dictIndexToButtonName[selectedNum] ].disabled = True
 			
 			
-			self.check_winner(self.enemy)
-			self.check_winner(self.player)
+			self.game.check_winner()
 			
 			
-			if (self.player.isWin or self.enemy.isWin or len(self.lstAvailableChoice) == 0):
-				self.isGameOver = True
+			if (self.player.isWin or self.enemy.isWin or len(self.game.lstAvailableChoice) == 0):
+				self.game.over()
 			
 			
 			self.enemy.isTurn = False
@@ -170,11 +131,12 @@ class TicTacToeGame(Widget):
 
 	def button_release(self, btn):
 		
-		if (self.isGameOver):
+		if (self.game.isOver):
+			
 			self.set_all_button_disable(True)
 			
 			boxLayout = BoxLayout(orientation = "vertical")
-
+			
 			dlgGameOver = Popup(title = "GameOver",
 					size_hint = (None, None),
 					size = (400, 400),
